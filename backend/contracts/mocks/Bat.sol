e320f4d31f02bb5167098dfe079304f799d781ae
Akash Vaghela <akashvaghela09@gmail.com>
2022-05-14 20:05:20 +0530

    mock token added

---
+++ backend/contracts/mocks/Bat.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BAT is ERC20 {
+
+    constructor() ERC20("BAT", "BAT Basic Authentication Token") public {}
+
+    function faucet(address _to, uint _amount) external {
+        _mint(_to, _amount);
+    }
+}
---
+++ backend/contracts/mocks/Dai.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Dai is ERC20 {
+
+    constructor() ERC20("Dai", "Dai StableCoin") public {}
+
+    function faucet(address _to, uint _amount) external {
+        _mint(_to, _amount);
+    }
+}
